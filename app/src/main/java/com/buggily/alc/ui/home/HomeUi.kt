72a8c82c8e40d97b3ecca3c0ae659e22d3df3df8
buggily <58127678+buggily@users.noreply.github.com>
2022-07-21 18:28:46 -0500

    remove view model

--- app/src/main/java/com/buggily/alc/ui/home/HomeUi.kt
+++ app/src/main/java/com/buggily/alc/ui/home/HomeUi.kt
@@ -5,20 +5,21 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun HomeScreen(viewModel: HomeViewModel = viewModel()) {
-    val value: String by viewModel.value.observeAsState(String())
+fun HomeScreen() {
+    var value: String by remember { mutableStateOf(String()) }
 
     HomeScreen(
         value = value,
-        onValueChange = { viewModel.onValueChange(it) },
+        onValueChange = { value = it },
     )
 }
 
