e3dc811844452852d0ea2e3481d22e1bdaaf130a
buggily <58127678+buggily@users.noreply.github.com>
2022-07-21 18:22:10 -0500

    remove saved state

--- app/src/main/java/com/buggily/alc/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/buggily/alc/ui/home/HomeViewModel.kt
@@ -2,19 +2,14 @@ package com.buggily.alc.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel(private val state: SavedStateHandle) : ViewModel() {
+class HomeViewModel : ViewModel() {
 
-    private val _value: MutableLiveData<String> = state.getLiveData(VALUE, String())
+    private val _value: MutableLiveData<String> = MutableLiveData(String())
     val value: LiveData<String> = _value
 
     fun onValueChange(value: String) {
-        state[VALUE] = value
-    }
-
-    private companion object {
-        private const val VALUE = "value"
+        _value.value = value
     }
 }
